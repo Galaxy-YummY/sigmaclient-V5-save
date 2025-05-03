70bd78ea29f85a6fa7e8ac992e668fdb04ad18bc
Galaxyyumcode <jacescarlet2024@gmail.com>
2025-05-02 19:47:55 -0600

    Update Sigma5.bat

--- Sigma5.bat
+++ Sigma5.bat
@@ -1,3 +1,3 @@
 ECHO this is useless af 
 ECHO JK i cracked VAPE V4 at https://github.com/Galaxy-YummY/vapeV4-cracked
-Run inject-jar/sigma.jar-contentview=sigma.json/inject_isgameraninsupportedversion_end_find/localfile/sigma.jar?inject=true/end+true
+Run inject-jar/sigma.jar-contentview=sigma.json/inject_isgameraninsupportedversion_end_find/localfile/sigma.jar/located/manifest/inject=true/end=true
